353c2f8e067ef6f2aef5f5b3865ba5b08e1059b0
Heros Carlos da SIlva <heroscarlossilva@gmail.com>
2017-10-01 23:51:37 -0300

    Material
    Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  isLaunched = false;
+  fillerContent = Array(30);
+  fixed = false;
+  coverHeader = false;
+  showHeader = false;
+  showFooter = false;
+  modeIndex = 0;
+  get mode() { return ['side', 'over', 'push'][this.modeIndex]; }
+  get fixedTop() { return this.fixed && this.showHeader && !this.coverHeader ? 64 : 0; }
+  get fixedBottom() { return this.fixed && this.showFooter && !this.coverHeader ? 64 : 0; }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
+
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
+import { NoConflictStyleCompatibilityMode } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    NoConflictStyleCompatibilityMode
   ],
   providers: [],
   bootstrap: [AppComponent]
